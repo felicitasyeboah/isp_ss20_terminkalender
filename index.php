ad5e57bf8a7872e41ff05c7ad028c65037b7364a
Christian Till <chris.till@web.de>
2020-07-18 14:59:14 +0200

    Matrikelnummer ergaenzt

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!--
 Semester Projekt in dem Modul ISP SS20
-TODO Autoren: Christian Till - Matrikelnr. ???  und Felicitas Yeboah - Matrikelnr. 290784
+TODO Autoren: Christian Till - Matrikelnr. 238872  und Felicitas Yeboah - Matrikelnr. 290784
 TODO Abgabedatum  02.08.2020
 -->
 <?php
@@ -15,9 +15,9 @@ include "Kalender.php";
 <head>
     <link rel="stylesheet" href="css/styles.css">
     <meta name="author" content="Felicitas Yeboah - Martrikelnummer: 290784">
-    <meta name="author" content="Christian Till - Martrikelnummer: ???">
-    <meta name="description" content="ISP - Projekt Terminkalender Gruppe 16 - SS20 - Felicitas Yeboah - 290784 und Christian Till">
-    <title>ISP - Projekt Terminkalender Gruppe 16 - SS20 - Felicitas Yeboah - 290784 und Christian Till</title>
+    <meta name="author" content="Christian Till - Martrikelnummer: 238872">
+    <meta name="description" content="ISP - Projekt Terminkalender Gruppe 16 - SS20 - Felicitas Yeboah - 290784 und Christian Till - 238872">
+    <title>ISP - Projekt Terminkalender Gruppe 16 - SS20 - Felicitas Yeboah - 290784 und Christian Till - 238872</title>
 
 </head>
 <body>
