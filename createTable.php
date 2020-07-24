c3612dcdda92edadb6246b2d7b2d9bb530405626
Christian Till <till@ahpgmbh.local>
2020-07-24 14:56:15 +0200

    Tabelle Kategorie - Feld farbe fuer Hex-Werte

--- createTable.php
+++ createTable.php
@@ -31,15 +31,15 @@ try {
     $createTable = "CREATE TABLE IF NOT EXISTS `kategorie` ( 
         `id` INT NOT NULL AUTO_INCREMENT , 
         `name` VARCHAR(65) NOT NULL , 
-        `farbe` TINYINT(1) NOT NULL DEFAULT '0' , 
+        `farbe` CHAR(6) NOT NULL , 
          PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_bin;";
     if($db->exec($createTable)) {
         echo "Tabelle angelegt.";
     }
-    /*$sql = "INSERT INTO `kalender` (`id`, `anfang`, `ende`, `ganztag`, `titel`, `beschreibung`, `ort`, `kategorie`, `farbe`) VALUES (NULL, '2020-07-15 07:00:00', '2020-07-15 09:00:00', '0', 'Zweiter Testtermin', 'Das ist der zweite Testtermin, der über die php-funktion eingetragen wurde', 'da wo ich wohne', 'privat', '0');";
+    $sql = "INSERT INTO `kategorie` (`id`, `name`, `farbe`) VALUES (NULL, 'privat', '9DDF20');";
     if($db->exec($sql)) {
         echo "TestTermin erfolgreich eingetragen.";
-    }*/
+    }
 } catch (PDOException $e) {
     // Nachricht bei Fehler
     exit("Fehler beim anlegen der Datenbank-Tabelle kategorie!" .
