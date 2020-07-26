2221b0e6227b80ef7049ea2a946ce1229bea89b6
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2020-07-26 13:53:24 +0200

    getAllEvents Methode angepasst, damit der Konstruktor funktioniert

--- Termin.php
+++ Termin.php
@@ -15,20 +15,40 @@ class Termin
     private $kategorie;
     private $farbe;
 
-    public function __construct() {
+/*    public function __construct() {
+        //TODO auslagern in eine Funktion und Konstruktor benutzen um einen Termin zu erzeugen
         //\\ holen des Kategorie-Datensatzes für diesen Termin
         if(isset($this->kategorieid)) {
             $this->kategorie = $GLOBALS["db"]->getKategorie($this->kategorieid);
             $this->farbe = $GLOBALS["db"]->getKategorie($this->farbe);
         }
+    }*/
 
+    public function __construct($titel, $beschreibung, $anfang, $ende, $ort, $kategorieid, $farbe, $ganztag = '0') {
 
+        $this->anfang = $anfang;
+        $this->ende = $ende;
+        $this->titel = $titel;
+        $this->beschreibung = $beschreibung;
+        $this->ort = $ort;
+        $this->ganztag = $ganztag;
+        $this->farbe = $farbe;
+        $this->kategorieid = $kategorieid;
+        if(isset($this->kategorieid)) {
+            $this->kategorie = $GLOBALS["db"]->getKategorie($this->kategorieid);
+            $this->farbe = $farbe;
+
+            ///$this->farbe = $GLOBALS["db"]->getKategorie($this->farbe);
+
+            //$this->farbe = $farbe;
+        }
+        echo "Termin erstellt";
     }
     public function __set($name, $value) {}
     //TODO hier wird nen Fehler angezeigt  beim hinzufuegen von termien deshalb auskommentiert
     //public function __get($name) {return $this->$name;}
 
-    public function terminErstellen($titel, $beschreibung, $anfang, $ende, $ort, $kategorieid, $farbe, $ganztag = '0') {
+/*    public function terminErstellen($titel, $beschreibung, $anfang, $ende, $ort, $kategorieid, $farbe, $ganztag = '0') {
 
         $this->anfang = $anfang;
         $this->ende = $ende;
@@ -43,7 +63,7 @@ class Termin
             $this->farbe = $farbe;
         }
         echo "Termin erstellt";
-    }
+    }*/
     //\\ gibt den Termin in HTML-Ansicht zurück
     public function toHTML(): string {
         $html  = '<div class="event"';
